8af5184ffde3892f367f64f2428186d8bb64c08f
jose-ng <jose.ng@globant.com>
2024-06-25 16:56:01 -0600

    Fix typo error

--- routes/user.router.js
+++ routes/user.router.js
@@ -14,8 +14,6 @@ const userService = new UserService();
 router.get('/',
   async (req, res, next) => {
     try {
-      const a = null;
-      a[0][3] = 1;
       const { query, page, limit } = req.query;
       const result = await userService.getAll(query, page, limit);
       res.json(result);
